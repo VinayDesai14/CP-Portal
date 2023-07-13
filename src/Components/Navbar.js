62749a1ff3040427ee0da52e72efc39bef4d5ea0
Shivam <me210003070@iiti.ac.in>
2023-07-13 16:48:18 +0530

    login signup page and bookmark functionality and data transfer

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,11 +7,12 @@ import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import logo from './logo.ico';
 import { alignProperty } from '@mui/material/styles/cssUtils';
+import { useAuth0 } from "@auth0/auth0-react";
 
 function Navbar() {
 
    const [flag,setFlag]=useState(false);
-
+   const {user, loginWithRedirect, isAuthenticated, logout} = useAuth0();
    
   return (
     <>
@@ -24,14 +25,23 @@ function Navbar() {
            
           <Button className='home' component={NavLink} to='/' style={({ isActive }) => { return { backgroundColor: isActive ? '' : '' } }} sx={{ color: 'white', textTransform: 'none', paddingRight:'20px',fontFamily:"'Girassol', cursive",fontSize:18}}>Home</Button>
 
-          <Button component={NavLink} to='/todolist' style={({ isActive }) => { return { backgroundColor: isActive ? '' : '' } }} sx={{ color: 'white', textTransform: 'none', paddingRight:'20px',fontFamily:"'Girassol', cursive",fontSize:18 }} className='dim pointer'>ToDoList</Button>
-
+          {isAuthenticated ? (
+          <Button component={NavLink} to='/todolist' style={({ isActive }) => { return { backgroundColor: isActive ? '' : '' } }} sx={{ color: 'white', textTransform: 'none' , paddingRight:'20px',fontFamily:"'Girassol', cursive",fontSize:18}} className='dim pointer'>ToDoList</Button>
+          ):
+            <button onClick={() => loginWithRedirect()}>ToDoList</button>
+          }
           <Button component={NavLink} to='/contests' style={({ isActive }) => { return { backgroundColor: isActive ? '' : '' } }} sx={{ color: 'white', textTransform: 'none',paddingRight:'20px',fontFamily:"'Girassol', cursive" ,fontSize:18}} className='dim pointer'>Contests</Button>
 
           <Button component={NavLink} to='/todolist' style={({ isActive }) => { return { backgroundColor: isActive ? '' : '' } }} sx={{ color: 'white', textTransform: 'none' , paddingRight:'20px',fontFamily:"'Girassol', cursive",fontSize:18}} className='dim pointer'>Favorite</Button>
 
-          <Button   component={NavLink} to='/loginorsignup' style={({ isActive }) => { return { backgroundColor: isActive ? '' : '' } }} sx={{ color: 'white', textTransform: 'none',marginLeft:'auto',textDecoration:'underline',fontFamily:"'Girassol', cursive",fontSize:18}} className='dim pointer'>Login/SignUp</Button>
-
+          {/* <Button   component={NavLink} to='/loginorsignup' style={({ isActive }) => { return { backgroundColor: isActive ? '' : '' } }} sx={{ color: 'white', textTransform: 'none',marginLeft:'auto',textDecoration:'underline',fontFamily:"'Girassol', cursive",fontSize:18}} className='dim pointer'>Login/SignUp</Button> */}
+          {/* <Button onClick={() => loginWithRedirect()} component={NavLink} to='/loginorsignup' style={({ isActive }) => { return { backgroundColor: isActive ? '' : '' } }} sx={{ color: 'white', textTransform: 'none',marginLeft:'auto',textDecoration:'underline',fontFamily:"'Girassol', cursive",fontSize:18}} className='dim pointer'>Login/SignUp</Button> */}
+          {isAuthenticated && (<p>{user.name}</p>)}
+          {isAuthenticated ? (
+            <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Log Out</button>
+          ):
+            <button onClick={() => loginWithRedirect()}>Log In</button>
+          }
         </Toolbar>
       </AppBar>
     </Box>
--- src/Components/Topics/BinarySearch.js
+++ src/Components/Topics/BinarySearch.js
@@ -1,11 +1,38 @@
 /* eslint-disable no-unused-vars */
 import { Box, Button, Grid, Stack, Typography } from '@mui/material'
-import React, { useEffect, useState } from 'react'
-import Axios from 'axios'
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 import TurnedInNotIcon from '@mui/icons-material/TurnedInNot';
-
+import { useAuth0 } from "@auth0/auth0-react";
+import { useNavigate } from 'react-router-dom';
 function BinarySearch() {
+  const {user, loginWithRedirect, isAuthenticated, logout} = useAuth0();
+  async function setData(name,link){
+    const email=user.email;
+    try{
 
+      await Axios.post("http://localhost:8000/todolistdata",{
+          email,link,name
+      })
+      .then(res=>{
+        if(res.data==="exist"){
+            alert("Problem already saved");
+          }
+        else if(res.data==="notexist"){
+          alert("Problem saved successfully");
+        }
+      })
+      .catch(e=>{
+          alert("wrong details");
+          console.log(e);
+      })
+
+  }
+  catch(e){
+      console.log(e);
+  }
+  }
+  
   const url = "https://codeforces.com/api/problemset.problems?tags=binary%20search";
   const [problemset, setProblemsset] = useState([]);
   const btnProps = {
@@ -43,7 +70,11 @@ function BinarySearch() {
                 <Button variant='outlined' href={`https://codeforces.com/problemset/problem/${contestId}/${index}`} sx={btnProps} target='_blank'>Solve Problem</Button>
               </Grid>
               <Grid item lg={1} md={1} sm={2} xs={2} sx={{mt:2}}>
-                <TurnedInNotIcon />
+                {isAuthenticated ?(
+                  <Button onClick={()=>setData(name,`https://codeforces.com/problemset/problem/${contestId}/${index}`)}>save</Button>
+                ):
+                  <Button onClick={() => loginWithRedirect()}>Save</Button>
+                }
               </Grid>
             </Grid>
 
